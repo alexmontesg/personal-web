49a6fb89da6345b8b57bb67ce9cda1e7e1b1ca34
Alejandro <alejandro.montes.g@gmail.com>
2013-09-14 16:57:43 +0200

    Added pagination

--- blog.php
+++ blog.php
@@ -1,6 +1,24 @@
 <?php
 	include_once 'includes/db_connect.php';
-	$query = $db -> prepare("SELECT post_id, title, LEFT(body, 1000) AS body, category FROM posts INNER JOIN categories ON categories.category_id=posts.category_id ORDER BY posted DESC");
+	$query = $db -> query('SELECT COUNT(*) AS number FROM posts');
+	$post_count = $query -> fetch_object() -> number;
+	$per_page = 5;
+	$pages = ceil($post_count / $per_page);
+	if(isset($_GET['page']) && is_numeric($_GET['page'])) {
+		$page = $_GET['page'];
+	} else {
+		$page = 1;
+	}
+	if($page < 1) {
+		$page = 1;
+	}
+	if($page > $pages) {
+		$page = $pages;
+	}
+	$start = ($page - 1) * $per_page;
+	$prev = $page - 1;
+	$next = $page + 1;
+	$query = $db -> prepare("SELECT post_id, title, LEFT(body, 1000) AS body, category FROM posts INNER JOIN categories ON categories.category_id=posts.category_id ORDER BY posted DESC LIMIT $start, $per_page");
 	$query -> execute();
 	$query -> bind_result($post_id, $title, $body, $category);
 ?>
@@ -76,6 +94,33 @@
 			</article>
 			<hr />
 			<?php endwhile; ?>
+			<div class="pagination-centered">
+				<ul class="pagination">
+					<?php
+					if($prev > 0 && $prev - 1 > 0) {
+					?>
+					<li class="arrow"><a href="blog">&laquo;</a></li>
+					<?php	
+					} if($prev > 0) {
+					?>
+					<li><a href="blog?page=<?php echo $prev; ?>"><?php echo $prev; ?></a></li>
+					<?php
+					}
+					?>
+					<li class="current"><a href="#"><?php echo $page; ?></a></li>
+					<?php
+					if($next <= $pages) {
+					?>
+					<li><a href="blog?page=<?php echo $next; ?>"><?php echo $next; ?></a></li>
+					<?php
+					} if($next < $pages) {
+					?>
+					<li class="arrow"><a href="blog?page=<?php echo $pages; ?>">&raquo;</a></li>
+					<?php
+					}
+					?>
+			</ul>
+			</div>
 		<?php include_once 'footer.php'; ?>
 		
 	</body>
